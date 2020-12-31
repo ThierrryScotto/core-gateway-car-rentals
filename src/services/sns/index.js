ff4740603af77062d0fbf37dd9f450cde1f8dc01
ThierrryScotto <thierry_192@outlook.com>
2020-12-30 22:37:25 -0300

    some adjustments

--- src/services/sns/index.js
+++ src/services/sns/index.js
@@ -5,7 +5,6 @@ AWS.config.update({
     region: 'us-east-1',
     accessKeyId: process.env.AWS_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-
 });
 
 const sendSMS = async (client, car) => {
